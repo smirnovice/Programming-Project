831ce127dba39935c1929d75dc9141b94e602736
smirnovice <asmirnov@tcd.ie>
2017-02-07 11:14:33 +0000

    Create Timer.java

---
+++ Timer.java
@@ -0,0 +1,28 @@
+package com.example.alex.programmingproject2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Alex on 31/01/2017.
+ */
+
+public class Sounds extends android.support.v4.app.Fragment
+{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.sounds, container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Sounds");
+    }
+}
